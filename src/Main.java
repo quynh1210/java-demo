9d0cd83fb09788cdd4593c310d80094fa022a93c
quynh1210 <quynh1210.nt@gmail.com>
2022-01-04 14:47:27 +0700

    add file

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 10;
+        float f = 20.5f;
+        double d = 20.5;
+        boolean b = true;
+        char c = 'a';
+        String s = "Hà Nội";
+    }
+}
+System.out.prinLn("i= "+i);
+System.out.prinLn("f= "+f);
+System.out.prinLn("d= "+d);
+System.out.prinLn("b= "+b);
+System.out.prinLn("c= "+c);
+System.out.prinLn("s= "+s);
